7185eb4b381efdc38aafcfb4c16f731ee3f592fb
Lisa Fabien <lillyfab26@icloud.com>
2020-09-27 03:03:58 -0400

    Initial Commit

---
+++ Flicks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flicks
+//
+//  Created by Lisa Fabien on 9/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
