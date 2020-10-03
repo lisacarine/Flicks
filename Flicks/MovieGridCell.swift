6192408c2e326a1c59fbff040b46c750bd42a329
Lisa Fabien <lillyfab26@icloud.com>
2020-10-02 20:42:46 -0400

    Part 2 of the Flicks app (adding details page and movie grid)

---
+++ Flicks/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flicks
+//
+//  Created by Lisa Fabien on 10/2/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+   
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
